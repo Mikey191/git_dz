88f6461ed9bcae26062c73328980289fde9018de
Mikey191 <Desertigorb@gmail.com>
2022-07-15 10:50:59 +0300

    int DZ

--- git_dz/Source.cpp
+++ git_dz/Source.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+using namespace std;
+
+int Sum(int &a, int &b) {
+	int sum = a + b;
+	return sum;
+}
+
+
+int main() {
+	int a, b;
+	cout << "Enter 2 elements: ";
+	cin >> a;
+	cin >> b;
+	cout << "Summ a + b = " << Sum(a,b) << endl;
+	return 0;
+}
